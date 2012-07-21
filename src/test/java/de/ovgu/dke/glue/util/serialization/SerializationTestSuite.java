3a2931a7ca1e3c496df4cbc2330421e8384fd83c
Sebastian Dorok <sebastian.dorok@st.ovgu.de>
2012-07-21 12:37:15 +0000

    [test] commented convenience class
    git-svn-id: https://projects.dke-research.de/svn/tools/glue/glue-util/trunk@194 75a1b36b-258a-4167-9528-f3dfdc565bb7

--- src/test/java/de/ovgu/dke/glue/util/serialization/SerializationTestSuite.java
+++ src/test/java/de/ovgu/dke/glue/util/serialization/SerializationTestSuite.java
@@ -4,6 +4,20 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+/**
+ * <p>
+ * This test suite is just for convenience to start all tests just with one
+ * call. It works well with the Eclipse J-Unit plugin.
+ * </p>
+ * <p>
+ * Surefire will ignore this suite during Maven test due to naming conventions
+ * (it doesn't start or end with "Test" but with "Tests"). This prevents running
+ * test cases twice during Maven test.
+ * </p>
+ * 
+ * @author Sebastian Dorok
+ * 
+ */
 @RunWith(Suite.class)
 @SuiteClasses({ SerializerCollectionProviderTest.class,
 		SingleSerializerProviderTest.class, NullSerializerTest.class })
