748963cfd5c09a745520a0a3f29c6816543f5cff
Stefan Haun <stefan.haun@ovgu.de>
2014-01-07 13:56:36 +0000

    Adapt to changed endpoint definition.
    git-svn-id: https://projects.dke-research.de/svn/tools/glue/glue-util/branches/shaun_0.3.0@460 75a1b36b-258a-4167-9528-f3dfdc565bb7

--- src/main/java/de/ovgu/dke/glue/util/transport/EndpointAdapter.java
+++ src/main/java/de/ovgu/dke/glue/util/transport/EndpointAdapter.java
@@ -7,9 +7,10 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import net.jcip.annotations.ThreadSafe;
-
+import de.ovgu.dke.glue.api.endpoint.Endpoint;
 import de.ovgu.dke.glue.api.transport.Connection;
-import de.ovgu.dke.glue.api.transport.Endpoint;
+import de.ovgu.dke.glue.api.transport.PacketHandler;
+import de.ovgu.dke.glue.api.transport.PacketThread;
 import de.ovgu.dke.glue.api.transport.TransportException;
 import de.ovgu.dke.glue.api.transport.TransportFactory;
 
@@ -27,14 +28,17 @@ import de.ovgu.dke.glue.api.transport.TransportFactory;
 @ThreadSafe
 public class EndpointAdapter implements Endpoint {
 	private final String id;
+	private final String schema;
 
 	private final Set<TransportFactory> transportFactories;
 
-	public EndpointAdapter(String id) {
+	public EndpointAdapter(String id, String schema) {
 		this.id = id;
 		if (id == null)
 			throw new NullPointerException("ID parameter must not be null!");
 
+		// TODO check schema parameter
+		this.schema = schema;
 		// TODO best solution for thread-safe set?
 		this.transportFactories = Collections
 				.newSetFromMap(new ConcurrentHashMap<TransportFactory, Boolean>());
@@ -46,17 +50,25 @@ public class EndpointAdapter implements Endpoint {
 	}
 
 	@Override
-	public Connection getConnection(URI peer, String schema)
+	public PacketThread openPacketThread(URI peer, PacketHandler handler)
 			throws TransportException {
+		Connection con = null;
+
 		// iterate the transport factories for the connection
 		for (TransportFactory factory : transportFactories)
-			if (factory.servesPeer(peer, schema))
-				return factory.createTransport(peer).getConnection(schema);
+			if (factory.servesPeer(peer, schema)) {
+				con = factory.createTransport(peer).getConnection(schema);
+				break;
+			}
 
 		// not found -> Exception
-		throw new NoSuchElementException(
-				"Registry does not contain a transport factory for peer "
-						+ peer.toASCIIString() + " with schema " + schema);
+		if (con == null)
+			throw new NoSuchElementException(String.format(
+					"Registry does not contain a transport factory "
+							+ "for peer %s with schema %s!",
+					peer.toASCIIString(), schema));
+
+		return con.createThread(handler);
 	}
 
 	/**
